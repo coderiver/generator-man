2592cda83f120a1ab8fd9b5a0cbb34a84c40471b
akella <akella.a@gmail.com>
2017-03-11 18:37:01 +0200

    add sass-lint for pre-commit

--- generators/app/templates/gulp/tasks/lint.js
+++ generators/app/templates/gulp/tasks/lint.js
@@ -2,6 +2,7 @@ var gulp        = require('gulp');
 var config      = require('../config');
 var htmlhint    = require('gulp-htmlhint');
 var gitStaged   = require("gulp-git-staged");
+var sassLint    = require('gulp-sass-lint')
 
 
 gulp.task('lint:js', function() {
@@ -17,7 +18,15 @@ gulp.task('lint:html', function() {
     .pipe(htmlhint('.htmlhintrc'))
     .pipe(htmlhint.failReporter());
 });
+
+gulp.task('lint:sass', function() {
+  return gulp.src('src/sass/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
 gulp.task('lint', [
-    // 'lint:js',
+    'lint:sass',
     'lint:html'
 ]);

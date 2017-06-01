3075130e5f5a47261aa54c2e37ca49e6367a0955
akella <akella.a@gmail.com>
2017-06-01 19:34:27 +0300

    alias'es for animation libraries GSAP-Scrollmagic etc 🍋

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -54,7 +54,16 @@ function createConfig(env) {
             })
         ],
         resolve: {
-            extensions: ['.js']
+            extensions: ['.js'],
+            alias: {
+                "TweenLite": path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+                "TweenMax": path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+                "TimelineLite": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+                "TimelineMax": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+                "ScrollMagic": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+                "animation.gsap": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+                "debug.addIndicators": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+            }
         },
         module: {
             rules: [{

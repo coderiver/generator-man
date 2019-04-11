4c19447f39a10a67c865161a9939a2fb8c280574
baqudo <aleks@coderiver.com.ua>
2019-04-11 16:43:21 +0300

    updated gulpfile.babel.js for readable output

--- generators/app/templates/gulpfile.babel.js
+++ generators/app/templates/gulpfile.babel.js
@@ -6,22 +6,22 @@ const getTaskWatch = task => require('./gulp/tasks/' + task).watch(gulp);
 
 gulp.task('clean', getTaskBuild('clean'));
 gulp.task('copy', getTaskBuild('copy'));
-gulp.task('server', () => getTaskBuild('server'));
-<% if (templates === 'nunjucks') { %>gulp.task('nunjucks', () => getTaskBuild('nunjucks'));<% } %>
-<% if (templates === 'pug') { %>gulp.task('pug', () => getTaskBuild('pug'));<% } %>
-<% if (css === 'sass') { %>gulp.task('sass', () => getTaskBuild('sass'));<% } %>
-<% if (sprites.indexOf('svg') !== -1) { %>gulp.task('sprite:svg', () => getTaskBuild('sprite-svg'));<% } %>
-<% if (svgo) { %>gulp.task('svgo', () => getTaskBuild('svgo'));<% } %>
-<% if (preview) { %>gulp.task('list-pages', getTaskBuild('list-pages'));<% } %>
+gulp.task('server', () => getTaskBuild('server'));<% if (templates === 'nunjucks') { %>
+gulp.task('nunjucks', () => getTaskBuild('nunjucks'));<% } %><% if (templates === 'pug') { %>
+gulp.task('pug', () => getTaskBuild('pug'));<% } %><% if (css === 'sass') { %>
+gulp.task('sass', () => getTaskBuild('sass'));<% } %><% if (sprites.indexOf('svg') !== -1) { %>
+gulp.task('sprite:svg', () => getTaskBuild('sprite-svg'));<% } %><% if (svgo) { %>
+gulp.task('svgo', () => getTaskBuild('svgo'));<% } %><% if (preview) { %>
+gulp.task('list-pages', getTaskBuild('list-pages'));<% } %>
 gulp.task('webpack', getTaskBuild('webpack'));
 
-gulp.task('copy:watch', getTaskWatch('copy'));
-<% if (templates === 'nunjucks') { %>gulp.task('nunjucks:watch', getTaskWatch('nunjucks'));<% } %>
-<% if (templates === 'pug') { %>gulp.task('pug:watch', () => getTaskWatch('pug'));<% } %>
-<% if (css === 'sass') { %>gulp.task('sass:watch', getTaskWatch('sass'));<% } %>
-<% if (sprites.indexOf('svg') !== -1) { %>gulp.task('sprite:svg:watch', getTaskWatch('sprite-svg'));<% } %>
-<% if (svgo) { %>gulp.task('svgo:watch', getTaskWatch('svgo'));<% } %>
-<% if (preview) { %>gulp.task('list-pages:watch', getTaskWatch('list-pages'));<% } %>
+gulp.task('copy:watch', getTaskWatch('copy'));<% if (templates === 'nunjucks') { %>
+gulp.task('nunjucks:watch', getTaskWatch('nunjucks'));<% } %><% if (templates === 'pug') { %>
+gulp.task('pug:watch', getTaskWatch('pug'));<% } %><% if (css === 'sass') { %>
+gulp.task('sass:watch', getTaskWatch('sass'));<% } %><% if (sprites.indexOf('svg') !== -1) { %>
+gulp.task('sprite:svg:watch', getTaskWatch('sprite-svg'));<% } %><% if (svgo) { %>
+gulp.task('svgo:watch', getTaskWatch('svgo'));<% } %><% if (preview) { %>
+gulp.task('list-pages:watch', getTaskWatch('list-pages'));<% } %>
 gulp.task('webpack:watch', getTaskWatch('webpack'));
 
 const setmodeProd = done => {
@@ -40,14 +40,14 @@ gulp.task(
   'build',
   gulp.series(
     setmodeProd,
-    'clean',
-    <% if (sprites.indexOf('svg') !== -1) { %>'sprite:svg',<% } %>
-    <% if (svgo) { %>'svgo',<% } %>
-    <% if (css === 'sass') { %>'sass',<% } %>
-    <% if (templates === 'nunjucks') { %>'nunjucks',<% } %>
-    <% if (templates === 'pug') { %>'pug',<% } %>
-    'webpack',
-    <% if (preview) { %>'list-pages',<% } %>
+    'clean',<% if (sprites.indexOf('svg') !== -1) { %>
+    'sprite:svg',<% } %><% if (svgo) { %>
+    'svgo',<% } %><% if (css === 'sass') { %>
+    'sass',<% } %><% if (templates === 'nunjucks') { %>
+    'nunjucks',<% } %><% if (templates === 'pug') { %>
+    'pug',<% } %>
+    'webpack',<% if (preview) { %>
+    'list-pages',<% } %>
     'copy'
   )
 );
@@ -56,14 +56,14 @@ gulp.task(
   'build:dev',
   gulp.series(
     setmodeDev,
-    'clean',
-    <% if (sprites.indexOf('svg') !== -1) { %>'sprite:svg',<% } %>
-    <% if (svgo) { %>'svgo',<% } %>
-    <% if (css === 'sass') { %>'sass',<% } %>
-    <% if (templates === 'nunjucks') { %>'nunjucks',<% } %>
-    <% if (templates === 'pug') { %>'pug',<% } %>
-    'webpack',
-    <% if (preview) { %>'list-pages',<% } %>
+    'clean',<% if (sprites.indexOf('svg') !== -1) { %>
+    'sprite:svg',<% } %><% if (svgo) { %>
+    'svgo',<% } %><% if (css === 'sass') { %>
+    'sass',<% } %><% if (templates === 'nunjucks') { %>
+    'nunjucks',<% } %><% if (templates === 'pug') { %>
+    'pug',<% } %>
+    'webpack',<% if (preview) { %>
+    'list-pages',<% } %>
     'copy'
   )
 );
@@ -71,14 +71,14 @@ gulp.task(
 gulp.task(
   'watch',
   gulp.parallel(
-    'copy:watch',
-    <% if (templates === 'nunjucks') { %>'nunjucks:watch',<% } %>
-    <% if (templates === 'pug') { %>'pug:watch',<% } %>
-    <% if (sprites.indexOf('svg') !== -1) { %>'sprite:svg:watch',<% } %>
-    <% if (svgo) { %>'svgo:watch',<% } %>
-    <% if (preview) { %>'list-pages:watch',<% } %>
-    'webpack:watch',
-    <% if (css === 'sass') { %>'sass:watch'<% } %>
+    'copy:watch',<% if (templates === 'nunjucks') { %>
+    'nunjucks:watch',<% } %><% if (templates === 'pug') { %>
+    'pug:watch',<% } %><% if (sprites.indexOf('svg') !== -1) { %>
+    'sprite:svg:watch',<% } %><% if (svgo) { %>
+    'svgo:watch',<% } %><% if (preview) { %>
+    'list-pages:watch',<% } %>
+    'webpack:watch',<% if (css === 'sass') { %>
+    'sass:watch'<% } %>
   )
 );
 
